5e0cad9e779bccc112754939471cbb2c73f43827
jirikadlec2 <jirikadlec2@gmail.com>
2015-12-01 16:38:07 -0700

    re-add converter app

--- tethysapp-api/tethysapp/api/controllers.py
+++ tethysapp-api/tethysapp/api/controllers.py
@@ -38,4 +38,5 @@ def list_apps(request):
             'max_series': 1,
             'icon': 'http://appsdev.hydroshare.org/static/ts_converter/images/hydro.gif',
             }
-    return JsonResponse([app1], safe=False)
+    resp = [app1, app2]
+    return JsonResponse(resp, safe=False)
