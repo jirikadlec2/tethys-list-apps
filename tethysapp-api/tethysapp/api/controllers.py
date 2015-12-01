878548bbdcd52ae2995f316edab1527f74a71356
jirikadlec2 <jirikadlec2@gmail.com>
2015-12-01 16:09:47 -0700

    updated the api - enabled only the viewer

--- tethysapp-api/tethysapp/api/controllers.py
+++ tethysapp-api/tethysapp/api/controllers.py
@@ -9,32 +9,33 @@ def home(request):
     Controller for the app home page.
     """
     context = {}
-
     return render(request, 'api/home.html', context)
+
 
 def list_apps_help(request):
     """
     Controller for the list_apps_help page.
     """
     context = {}
-
     return render(request, 'api/list_apps.html', context)
+
 
 def list_apps(request):
     """
     Controller for the list_apps page.
     """
-    return JsonResponse({"apps":[{"name": "Time Series Viewer",
-                         'url': 'http://appsdev.hydroshare.org/apps/timeseries-viewer',
-                         'description': 'View graph and descriptive statistics of one or more time series',
-                         'min_series': 1,
-                         'max_series': 10,
-                         'icon': 'http://appsdev.hydroshare.org/static/timeseries_viewer/images/viewer_icon2.gif'},
-                        {"name": "Time Series Converter",
-                         'url': 'http://appsdev.hydroshare.org/apps/ts-converter',
-                         'description': 'Convert time series to daily, weekly, monthly, or yearly aggregate',
-                         'min_series': 1,
-                         'max_series': 1,
-                         'icon': 'http://appsdev.hydroshare.org/static/ts_converter/images/hydro.gif',
-                        }]}
-                        )
+    app1 = {'name': 'Time Series Viewer',
+            'url': 'http://appsdev.hydroshare.org/apps/timeseries-viewer',
+            'description': 'View graph and descriptive statistics of one time series',
+            'min_series': 1,
+            'max_series': 1,
+            'icon': 'http://appsdev.hydroshare.org/static/timeseries_viewer/images/viewer_icon2.gif'}
+
+    app2 = {'name': 'Time Series Converter',
+            'url': 'http://appsdev.hydroshare.org/apps/ts-converter',
+            'description': 'Convert time series to daily, weekly, monthly, or yearly aggregate',
+            'min_series': 1,
+            'max_series': 1,
+            'icon': 'http://appsdev.hydroshare.org/static/ts_converter/images/hydro.gif',
+            }
+    return JsonResponse([app1], safe=False)
